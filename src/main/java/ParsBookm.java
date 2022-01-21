4eeed79bed5ab3295349c03618e682500235ef92
Luxxrom <Rom9476@gmail.com>
2022-01-21 15:58:02 +0500

    sber1

--- src/main/java/ParsBookm.java
+++ src/main/java/ParsBookm.java
@@ -2,13 +2,13 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ParsBookm {
 
@@ -24,6 +24,7 @@ public class ParsBookm {
 //        webClient.getOptions().setThrowExceptionOnScriptError(false);
 //        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
 //        webClient.getOptions().setTimeout(5000);
+//        webClient.getOptions().setUseInsecureSSL(true);
 //        HtmlPage htmlPage = webClient.getPage(url);
 //        webClient.waitForBackgroundJavaScript(10000);
 //
@@ -41,7 +42,53 @@ public class ParsBookm {
 //        }
 
 
-        String url = "https://sbermegamarket.ru/";
+//        String url = "https://sbermegamarket.ru/";
+//        String url = "https://sbermegamarket.ru/catalog/kofe-v-zernah/";
+//        System.out.println("Loading page now ----------------------------------" + url);
+//
+//        // HtmlUnit
+//        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+//        webClient.getOptions().setJavaScriptEnabled(true);
+//        webClient.getOptions().setCssEnabled(false);
+//        webClient.getOptions().setThrowExceptionOnScriptError(false);
+//        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+//        webClient.getOptions().setTimeout(5000);
+//        webClient.getOptions().setUseInsecureSSL(true);
+//        HtmlPage htmlPage = webClient.getPage(url);
+//        webClient.waitForBackgroundJavaScript(10000);
+//
+//        String pageAsXml = htmlPage.asXml();
+//
+//        // Jsoup parsing
+//        Document page = Jsoup.parse(pageAsXml);
+//        Element mainEl = page.selectFirst("main");
+////        Elements divElements = mainEl.select("div[class=catalog-listing__items]");
+//        Elements divElements = mainEl.select("div");
+//        for (Element divEl : divElements) {
+////            System.out.println(divEl);
+//
+//            Elements divElements1 = divEl.select("div[class=catalog-listing__items]");
+//            for (Element divEl1 : divElements1) {
+////                System.out.println(divEl1);
+////                if (true) break;
+//
+////                Elements divElements2 = divEl1.select("div.item-info[div.item-money.span, div.item-title mobile-only.a]");
+//                Elements divElements2 = divEl1.select("div[class=item-info] span");
+//
+////                Elements divElements2 = divEl1.select("a[class=ddl_product_link]");
+//                int i = 0;
+//                for (Element divEl2 : divElements2) {
+////                    System.out.println(divEl2.text());
+////                    if (true) break;
+//                    System.out.println( String.valueOf(i++) + " " + divEl2.text());
+////                    System.out.println(divEl1.text());
+//
+////                System.out.println(divEl.text());
+//                }
+//            }
+//        }
+
+        String url = "https://sbermegamarket.ru/catalog/kofe-v-zernah/";
         System.out.println("Loading page now ----------------------------------" + url);
 
         // HtmlUnit
@@ -51,6 +98,7 @@ public class ParsBookm {
         webClient.getOptions().setThrowExceptionOnScriptError(false);
         webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
         webClient.getOptions().setTimeout(5000);
+        webClient.getOptions().setUseInsecureSSL(true);
         HtmlPage htmlPage = webClient.getPage(url);
         webClient.waitForBackgroundJavaScript(10000);
 
@@ -58,26 +106,48 @@ public class ParsBookm {
 
         // Jsoup parsing
         Document page = Jsoup.parse(pageAsXml);
-//        Elements divElement = page.select("div[class=content]");
-        Elements divElements = page.select("div[class=catalog-listing__items]");
-//        System.out.println(divElement.text());
-        for (Element divEl : divElements) {
-//            String[] elPoints = divEl.text().split("\\.");
-//            for (String elPoint : elPoints) {
-//                System.out.println(elPoint);
-            Elements divElements1 = page.select("div[class=item-title]");
-            for (Element divEl1 : divElements1) {
+        Element mainEl = page.selectFirst("main");
 
-                Elements divElements2 = page.select("a");
-                for (Element divEl2 : divElements2) {
+        Elements elementsDiv = listDiv(mainEl);
+//        Elements divElements = mainEl.select("div[class=catalog-listing__items]");
+//        Elements divElements = mainEl.select("div");
+//        for (Element divEl : divElements) {
+////            System.out.println(divEl);
 
-                    System.out.println(divEl2.text());
-//                    System.out.println(divEl1.text());
 
-//                System.out.println(divEl.text());
-                }
-            }
+//            Elements divElements1 = divEl.select("div[class=catalog-listing__items]");
+//            for (Element divEl1 : divElements1) {
+////                System.out.println(divEl1);
+////                if (true) break;
+//
+////                Elements divElements2 = divEl1.select("div.item-info[div.item-money.span, div.item-title mobile-only.a]");
+//                Elements divElements2 = divEl1.select("div[class=item-info] span");
+//
+////                Elements divElements2 = divEl1.select("a[class=ddl_product_link]");
+//                int i = 0;
+//                for (Element divEl2 : divElements2) {
+////                    System.out.println(divEl2.text());
+////                    if (true) break;
+//                    System.out.println( String.valueOf(i++) + " " + divEl2.text());
+////                    System.out.println(divEl1.text());
+//
+////                System.out.println(divEl.text());
+//                }
+//            }
+    }
+
+    static Elements listDiv(Element node) {
+
+        Attributes attributeEl = node.attributes();
+        for (Attribute attEl : attributeEl) {
+            System.out.print(attEl);
+        }
+        System.out.println("  " + node.text().toUpperCase());
+        Elements nodeEl = node.children();
+        for (Element nodeEl1 : nodeEl) {
+            listDiv(nodeEl1);
         }
 
+        return nodeEl;
     }
 }
